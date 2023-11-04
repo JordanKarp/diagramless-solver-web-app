2c9a88ab9454604b681fac9205378564f88b7fe9
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-11-04 15:10:00 -0700

    Update main.py

--- solver/main.py
+++ solver/main.py
@@ -5,21 +5,24 @@ from solver.value import SYMMETRIES
 
 
 def run(rows, columns, acrossClues, downClues, symmetry, starting, connected, findAll):
-    dimensions = (int(rows), int(columns))
-    acrossClueNumbers = [int(x) for x in acrossClues.split(" ")]
-    downClueNumbers = [int(x) for x in downClues.split(" ")]
-    cluestring = process_cluestring(acrossClueNumbers, downClueNumbers)
-    symmetryFormatted = SYMMETRIES[symmetry]
-    startingSquare = int(starting)
-    useSymmetry = symmetry not in ["", "Asymmetry"]
-    useStarting = startingSquare != 0
+    try:
+        dimensions = (int(rows), int(columns))
+        acrossClueNumbers = [int(x) for x in acrossClues.split(" ")]
+        downClueNumbers = [int(x) for x in downClues.split(" ")]
+        cluestring = process_cluestring(acrossClueNumbers, downClueNumbers)
+        symmetryFormatted = SYMMETRIES[symmetry]
+        startingSquare = int(starting)
+        useSymmetry = symmetry not in ["", "Asymmetry"]
+        useStarting = startingSquare != 0
 
-    puzzle_details = PuzzleDetails(
-        dimensions, cluestring, symmetryFormatted, startingSquare
-    )
-    solver_details = SolverDetails(
-        useSymmetry, useStarting, findAll, connected, True, False
-    )
-    puzzle = Puzzle(puzzle_details, solver_details)
-    solutions = puzzle.solve()
-    return solutions.return_grids()
+        puzzle_details = PuzzleDetails(
+            dimensions, cluestring, symmetryFormatted, startingSquare
+        )
+        solver_details = SolverDetails(
+            useSymmetry, useStarting, findAll, connected, True, False
+        )
+        puzzle = Puzzle(puzzle_details, solver_details)
+        solutions = puzzle.solve()
+        return solutions.return_grids()
+    except Exception:
+        return []
